79dfcc4e14f4f4e1676dfb0f546a5865b90db84c
pavnsai <pavansai18.a@gmail.com>
2024-11-19 12:06:57 -0700

    formated code and restructured a bit

--- frontend/image-processing-frontend/src/App.js
+++ frontend/image-processing-frontend/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, {useState} from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import ImageUploader from './components/ImageUploader/ImageUploader';
 import Header from "./components/Header/Header";
@@ -13,19 +13,19 @@ const App = () => {
     return (
         <Router>
             <div className="App">
-                <Header />
+                <Header/>
                 <div className="main-content">
                     <Routes>
-                        <Route path="/processed/:UUID" element={<ImageFetcher />} />
+                        <Route path="/processed/:UUID" element={<ImageFetcher/>}/>
                         <Route
                             path="/processed"
-                            element={<ImageFetcher images={images} />}
+                            element={<ImageFetcher images={images}/>}
                         />
-                        <Route path="/upload" element={<ImageUploader />} />
-                        <Route path="/" element={<HomePage />} />
+                        <Route path="/upload" element={<ImageUploader/>}/>
+                        <Route path="/" element={<HomePage/>}/>
                     </Routes>
                 </div>
-                <Footer />
+                <Footer/>
             </div>
         </Router>
     );
