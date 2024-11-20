6fce46fc55f0d4784d164461452ab1481d79af46
pavnsai <pavansai18.a@gmail.com>
2024-11-19 17:56:31 -0700

    added pubsub-secret.yaml for keda

--- backend/scripts.sh
+++ backend/scripts.sh
@@ -3,7 +3,7 @@ helm repo add kedacore https://kedacore.github.io/charts
 helm repo update
 helm install keda kedacore/keda --namespace keda --create-namespace
 
-
+kubectl apply -f pubsub-secret.yaml
 kubectl apply -f image_handler_deployment.yaml
 kubectl apply -f image_handler_service.yaml
 kubectl apply -f interaction_manager_deployment.yaml
